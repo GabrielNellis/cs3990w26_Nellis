229870a35473cc4daf40b77aeb3cbb47b41adcc4
GABRIEL NELLIS <5089401@nwpolytech.ca>
2026-04-07 02:21:44 -0600

    have a start (tomorrow's gonna be a LONG day)

---
+++ quizData.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Lisbon"],
+        correctAnswer: "Paris"
+    },
+    {
+        question: "Do you like JS?",
+        options: ["Yes", "No"],
+        correctAnswer: "Yes"
+    }
+    //more to be added later
+]
+
+const assets = [
+    
+]
